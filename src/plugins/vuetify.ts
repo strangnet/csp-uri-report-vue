a8b47abaa5b348d2328c2d32a848f6382d9a0ace
Patrick Strang <strangnet@gmail.com>
2020-03-01 01:44:16 +0100

    custom colors

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -3,4 +3,18 @@ import Vuetify from "vuetify/lib";
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: "#4caf50",
+        secondary: "#2196f3",
+        accent: "#ff5722",
+        error: "#f44336",
+        warning: "#ff9800",
+        info: "#3f51b5",
+        success: "#00bcd4"
+      }
+    }
+  }
+});
