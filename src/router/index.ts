8b069f023f486ee119e92e3aed6abf910a9a209b
Patrick Strang <strangnet@gmail.com>
2020-03-01 22:44:41 +0100

    preparing reports view

--- src/router/index.ts
+++ src/router/index.ts
@@ -16,13 +16,19 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    component: () => import(/* webpackChunkName: "about" */ "@/views/About.vue")
   },
   {
     path: "/reports",
     name: "Reports",
-    component: () => import("../views/ReportList.vue")
+    component: () =>
+      import(/* webpackChunkName: "report-list" */ "@/views/ReportList.vue")
+  },
+  {
+    path: "/report/:id",
+    name: "Report",
+    component: () =>
+      import(/* webpackChunkName: "report" */ "@/views/Report.vue")
   }
 ];
 
---
+++ src/views/Report.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container fluid>
+    <h1 class="display-2">Report {{ $route.params.id }}</h1>
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  name: "Report",
+  components: {
+    //
+  },
+  data: () => ({
+    //
+  })
+});
+</script>
