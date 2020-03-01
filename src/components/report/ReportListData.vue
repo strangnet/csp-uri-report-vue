f1f77a30ac80cab33dbb759f7fb07e86d22e0d8f
Patrick Strang <strangnet@gmail.com>
2020-03-02 00:04:09 +0100

    rudimentary api call and ui viz

--- src/components/report/ReportListData.vue
+++ src/components/report/ReportListData.vue
@@ -9,10 +9,16 @@
 
 <script lang="ts">
 import Vue from "vue";
+import ReportsService from "@/services/reports";
 export default Vue.extend({
   name: "ReportListData",
   components: {
     //
+  },
+  computed: {
+    reports() {
+      return this.$store.state.reports;
+    }
   },
   data: () => ({
     headers: [
@@ -36,55 +42,11 @@ export default Vue.extend({
         value: "documentURI",
         sortable: false
       }
-    ],
-    reports: [
-      {
-        id: "a880f3dc-5b99-4007-9d41-f95f15d006af",
-        createdAt: "2020-03-01T12:31:19.511041+01:00",
-        userAgent:
-          "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36",
-        blockedURI:
-          "https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js",
-        documentURI: "http://localhost:8080/"
-      },
-      {
-        id: "c624b529-cd00-4153-a8c5-0f1a4c527b94",
-        createdAt: "2020-03-01T12:31:19.511041+01:00",
-        userAgent:
-          "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36",
-        blockedURI:
-          "https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js",
-        documentURI: "http://localhost:8080/"
-      },
-      {
-        id: "2de62bbb-7503-4b94-b194-76f08292b3b9",
-        createdAt: "2020-03-01T12:31:19.511041+01:00",
-        userAgent:
-          "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36",
-        blockedURI:
-          "https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js",
-        documentURI: "http://localhost:8080/"
-      },
-      {
-        id: "a6d5d112-5d2d-4b68-8827-30af053489a6",
-        createdAt: "2020-03-01T12:31:19.511041+01:00",
-        userAgent:
-          "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36",
-        blockedURI:
-          "https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js",
-        documentURI: "http://localhost:8080/"
-      },
-      {
-        id: "7b2d8d71-fe74-4332-bbd1-7564b07d676b",
-        createdAt: "2020-03-01T12:31:19.511041+01:00",
-        userAgent:
-          "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36",
-        blockedURI:
-          "https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js",
-        documentURI: "http://localhost:8080/"
-      }
     ]
-  })
+  }),
+  created: () => {
+    ReportsService.getReports().then(res => console.log(res));
+  }
 });
 </script>
 
--- src/views/Report.vue
+++ src/views/Report.vue
@@ -1,18 +1,30 @@
 <template>
   <v-container fluid>
     <h1 class="display-2">Report {{ $route.params.id }}</h1>
+    {{ report.userAgent }}
   </v-container>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import ReportsService from "@/services/reports";
 export default Vue.extend({
   name: "Report",
   components: {
     //
   },
   data: () => ({
-    //
-  })
+    report: {}
+  }),
+  methods: {
+    loadReport() {
+      ReportsService.getReport(this.$route.params.id)
+        .then(res => (this.report = res.data))
+        .catch(err => console.error(err));
+    }
+  },
+  created() {
+    this.loadReport();
+  }
 });
 </script>
