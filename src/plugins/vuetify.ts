c52ebc30c5cb915eb9df150fe1349ec0b758a9ad
Patrick Strang <strangnet@gmail.com>
2020-03-01 01:58:10 +0100

    reusable theme definition

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -3,18 +3,21 @@ import Vuetify from "vuetify/lib";
 
 Vue.use(Vuetify);
 
+const theme = {
+  primary: "#4caf50",
+  secondary: "#2196f3",
+  accent: "#ff5722",
+  error: "#f44336",
+  warning: "#ff9800",
+  info: "#3f51b5",
+  success: "#00bcd4"
+};
+
 export default new Vuetify({
   theme: {
     themes: {
-      light: {
-        primary: "#4caf50",
-        secondary: "#2196f3",
-        accent: "#ff5722",
-        error: "#f44336",
-        warning: "#ff9800",
-        info: "#3f51b5",
-        success: "#00bcd4"
-      }
+      light: theme,
+      dark: theme
     }
   }
 });
