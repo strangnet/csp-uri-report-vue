ac5fe8cbe29736ce8c8683ede8fa2bd10a63257a
Patrick Strang <strangnet@gmail.com>
2020-03-12 22:23:15 +0100

    parsing the user agent

--- src/components/report/ReportListData.vue
+++ src/components/report/ReportListData.vue
@@ -4,16 +4,22 @@
     :items="reports"
     :items-per-page="5"
     class="elevation-1"
-  ></v-data-table>
+  >
+    <template #item.userAgent="{item}">
+      <UserAgentListDisplay :userAgent="item.userAgent" />
+    </template>
+  </v-data-table>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import ReportsService from "@/services/reports";
+import UserAgentListDisplay from "@/components/report/UserAgentListDisplay.vue";
+
 export default Vue.extend({
   name: "ReportListData",
   components: {
-    //
+    UserAgentListDisplay
   },
   computed: {
     reports() {
@@ -28,24 +34,26 @@ export default Vue.extend({
         value: "createdAt"
       },
       {
-        text: "User Agent",
+        text: "Browser",
         value: "userAgent",
         sortable: false
       },
       {
         text: "Blocked URI",
-        value: "blockedURI",
+        value: "blockedUri",
         sortable: false
       },
       {
         text: "Document URI",
-        value: "documentURI",
+        value: "documentUri",
         sortable: false
       }
     ]
   }),
-  created: () => {
-    ReportsService.getReports().then(res => console.log(res));
+  created() {
+    ReportsService.getReports().then(
+      res => (this.$store.state.reports = res.data)
+    );
   }
 });
 </script>
